2a58fa2c1eb2bc6e8770885f4f1eeb08daaf8a41
Terry623-dev <13750456@me.mcu.edu.tw>
2025-10-16 17:07:16 +0800

    week06

---
+++ week06/week06-4.cpp
@@ -0,0 +1,22 @@
+///week06-4.cpp
+///LeetCode學習計畫 Simulation 模擬, 地2題657. Robot Return to Origin
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0,y = 0;//一開始在原點O Origin
+        for(char c : moves) { /// C++進階迴圈
+            if(c=='U'){ //往上
+                y++;
+            }else if(c=='D'){///往下
+                y--;
+            }else if(c=='L'){///往左
+                x--;
+            }else if(c=='R'){///往右
+                x++;
+
+            }
+        }  ///最吼留在 原點(0,0) 嗎?
+        if(x==0 && y==0) return true;
+        else return false;
+    }
+};
